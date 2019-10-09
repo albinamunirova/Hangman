f0d4cd4b79ff9b5f5c72dc3887bd6f5ce2e2d1f4
albinamunirova <37842061+albinamunirova@users.noreply.github.com>
2019-10-09 19:05:14 +0300

    Update test_start.py

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,9 +1,11 @@
+"""This module tests the start of the game"""
+
 from game import start
 from mock import patch
 
 
 def test_start():
-
+    """This function tests the start of the game"""
     with patch('game.main') as perm_mock:
         perm_mock.return_value = True
         res = start.main()
